a3d754d78410e3d4213c47a6dea3f1d921447ee0
Rainame <wzxjjz@gmail.com>
2018-04-24 13:56:37 +0800

    fix bug.

--- dmzj-comic.js
+++ dmzj-comic.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         动漫之家助手
 // @namespace    https://manhua.dmzj.com/
-// @version      0.11
+// @version      0.12
 // @description  获取动漫之家被屏蔽的漫画目录及章节，脚本于Tampermonkey中测试通过。
 // @author       rainame
+// @license      MIT
 // @match        https://manhua.dmzj.com/*
-// @grant        unsafeWindow
 // @grant        GM_xmlhttpRequest
 // @require      https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
 // @require      https://cdn.bootcss.com/fancybox/3.1.20/jquery.fancybox.min.js
@@ -224,6 +224,21 @@
                         this.$list = $(list).appendTo( this.$grid ).on('click', 'li', function() {
                             instance.jumpTo($(this).data('index'));
                         });
+                    };
+                    FancyThumbs.update = function () {
+                        this.instance.$refs.container.toggleClass('fancybox-show-thumbs', this.isVisible);
+
+                        if (this.isVisible) {
+                            if (!this.$grid) { this.create(); }
+                            this.instance.trigger('onThumbsShow');
+                            $('div.fancybox-inner').css('right', '110px');
+                            this.focus();
+                        } else if (this.$grid) {
+                            this.instance.trigger('onThumbsHide');
+                            $('div.fancybox-inner').removeAttr('style');
+                        }
+
+                        this.instance.update();
                     };
                 }, 500);
             }
